cc7f8a358cd16de95f8962aa178e5af850682ead
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-12 20:10:15 -0300

    feat: generate new typings

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,133 +9,133 @@
 /* eslint-disable */
 
 export class NewLogin {
-  email?: Nullable<string>;
-  password?: Nullable<string>;
+    email?: Nullable<string>;
+    password?: Nullable<string>;
 }
 
 export class NewCharacter {
-  name?: Nullable<string>;
-  status?: Nullable<string>;
-  species?: Nullable<string>;
-  type?: Nullable<string>;
-  gender?: Nullable<string>;
-  originId?: Nullable<number>;
-  locationId?: Nullable<number>;
-  image?: Nullable<string>;
-  episode?: Nullable<Nullable<string>[]>;
-  url?: Nullable<string>;
-  created?: Nullable<string>;
+    name?: Nullable<string>;
+    status?: Nullable<string>;
+    species?: Nullable<string>;
+    type?: Nullable<string>;
+    gender?: Nullable<string>;
+    originId?: Nullable<number>;
+    locationId?: Nullable<number>;
+    image?: Nullable<string>;
+    episode?: Nullable<Nullable<string>[]>;
+    url?: Nullable<string>;
+    created?: Nullable<string>;
 }
 
 export class NewFavoritesList {
-  userId: number;
-  characterId: number;
+    userId: number;
+    characterId: number;
 }
 
 export class NewLocation {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 }
 
 export class NewOrigin {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 }
 
 export class NewUser {
-  name: string;
-  email: string;
-  password: string;
+    name: string;
+    email: string;
+    password: string;
 }
 
 export class AuthType {
-  user?: Nullable<User>;
-  token?: Nullable<string>;
+    user?: Nullable<User>;
+    token?: Nullable<string>;
 }
 
 export abstract class IMutation {
-  abstract login(data?: Nullable<NewLogin>): AuthType | Promise<AuthType>;
+    abstract login(data?: Nullable<NewLogin>): AuthType | Promise<AuthType>;
 
-  abstract createCharacter(
-    input?: Nullable<NewCharacter>,
-  ): Character | Promise<Character>;
+    abstract createCharacter(input?: Nullable<NewCharacter>): Character | Promise<Character>;
 
-  abstract createFavorites(
-    input?: Nullable<NewFavoritesList>,
-  ): FavoritesList | Promise<FavoritesList>;
+    abstract createFavorites(input?: Nullable<NewFavoritesList>): FavoritesList | Promise<FavoritesList>;
 
-  abstract createLocation(
-    input?: Nullable<NewLocation>,
-  ): Location | Promise<Location>;
+    abstract createLocation(input?: Nullable<NewLocation>): Location | Promise<Location>;
 
-  abstract createOrigin(input?: Nullable<NewOrigin>): Origin | Promise<Origin>;
+    abstract createOrigin(input?: Nullable<NewOrigin>): Origin | Promise<Origin>;
 
-  abstract createUser(input?: Nullable<NewUser>): User | Promise<User>;
+    abstract createUser(input?: Nullable<NewUser>): User | Promise<User>;
 }
 
 export class Character {
-  id: string;
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  originId?: Nullable<number>;
-  origin?: Nullable<Origin>;
-  locationId?: Nullable<number>;
-  location?: Nullable<Location>;
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
-  users?: Nullable<Nullable<FavoritesList>[]>;
+    id: string;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    originId?: Nullable<number>;
+    origin?: Nullable<Origin>;
+    locationId?: Nullable<number>;
+    location?: Nullable<Location>;
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
+    users?: Nullable<Nullable<FavoritesList>[]>;
 }
 
 export abstract class IQuery {
-  abstract character(): Character | Promise<Character>;
+    abstract character(id: string): Character | Promise<Character>;
 
-  abstract favoritesList(): FavoritesList | Promise<FavoritesList>;
+    abstract characteres(): Character[] | Promise<Character[]>;
 
-  abstract location(id: string): Location | Promise<Location>;
+    abstract favoritesList(id?: Nullable<string>): FavoritesList[] | Promise<FavoritesList[]>;
 
-  abstract locations(): Location[] | Promise<Location[]>;
+    abstract location(id: string): Location | Promise<Location>;
 
-  abstract origin(id: string): Origin | Promise<Origin>;
+    abstract locations(): Location[] | Promise<Location[]>;
 
-  abstract origins(): Origin[] | Promise<Origin[]>;
+    abstract locationByName(name?: Nullable<string>): Location | Promise<Location>;
 
-  abstract user(id: string): User | Promise<User>;
+    abstract origin(id: string): Origin | Promise<Origin>;
 
-  abstract userByEmail(email: string): User | Promise<User>;
+    abstract origins(): Origin[] | Promise<Origin[]>;
+
+    abstract originByName(name?: Nullable<string>): Origin | Promise<Origin>;
+
+    abstract user(id: string): User | Promise<User>;
+
+    abstract userByEmail(email: string): User | Promise<User>;
 }
 
 export class FavoritesList {
-  user?: Nullable<User>;
-  userId?: Nullable<number>;
-  character?: Nullable<Character>;
-  characterId?: Nullable<number>;
+    user?: Nullable<User>;
+    userId?: Nullable<number>;
+    character?: Nullable<Character>;
+    characterId?: Nullable<number>;
 }
 
 export class Location {
-  id: string;
-  name?: Nullable<string>;
-  url?: Nullable<string>;
-  characteres?: Nullable<Nullable<Character>[]>;
+    id: string;
+    name?: Nullable<string>;
+    url?: Nullable<string>;
+    characteres?: Nullable<Nullable<Character>[]>;
 }
 
 export class Origin {
-  id: string;
-  name?: Nullable<string>;
-  url?: Nullable<string>;
-  characteres?: Nullable<Nullable<Character>[]>;
+    id: string;
+    name?: Nullable<string>;
+    url?: Nullable<string>;
+    characteres?: Nullable<Nullable<Character>[]>;
 }
 
 export class User {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  characteres?: Nullable<Nullable<FavoritesList>[]>;
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    characteres?: Nullable<Nullable<FavoritesList>[]>;
 }
 
 type Nullable<T> = T | null;
